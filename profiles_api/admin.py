c7019d0b3c4a732734499819936838111a0b9e73
Rahul kumar Bharti <rkb6280@gmail.com>
2020-07-07 12:20:43 +0530

    Enable custome user

--- profiles_api/admin.py
+++ profiles_api/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from profiles_api.models import User_Profile
 # Register your models here.
+
+
+admin.site.register(User_Profile)
--- profiles_api/models.py
+++ profiles_api/models.py
@@ -1,3 +1,136 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import PermissionsMixin
+# from django.contrib.auth.models import BaseUserManager,AbstractBaseUser
+from django.contrib.auth.models import (
+    BaseUserManager, AbstractBaseUser
+)
 # Create your models here.
+
+class UserProfileManager(BaseUserManager):
+    """ manager for user profile"""
+    def create_user(self,email,name,password=None):
+        """ create new user profile"""
+        if not email:
+            raise ValueError('User Must Have An Email Address')
+        email=self.normalize_email(email)
+        user=self.model(email=email,name=name)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self,email,name,password):
+        """Create and save a new Superuser with given detail"""
+        user=self.create_user(email,name,password)
+        user.is_superuser=True
+        user.is_staff=True
+        user.save(using=self._db)
+
+        return user
+
+
+class User_Profile(AbstractBaseUser,PermissionsMixin):
+    """
+    database model for users in the system
+    """
+    email=models.EmailField(max_length=255,unique=True)
+    name=models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects =UserProfileManager()
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['name']
+
+    def get_full_name(self):
+        """
+        retrive full name of user
+        :return:
+        """
+        return self.name
+    def get_short_name(self):
+        """
+        retrive short name of user
+        :return:
+        """
+        return self.name
+
+
+    def __str__(self):
+        """ return string represtentation of our user"""
+        return self.email
+
+
+#
+# from django.db import models
+# from django.contrib.auth.models import (
+#     BaseUserManager, AbstractBaseUser
+# )
+#
+#
+# class MyUserManager(BaseUserManager):
+#     def create_user(self, email, date_of_birth, password=None):
+#         """
+#         Creates and saves a User with the given email, date of
+#         birth and password.
+#         """
+#         if not email:
+#             raise ValueError('Users must have an email address')
+#
+#         user = self.model(
+#             email=self.normalize_email(email),
+#             date_of_birth=date_of_birth,
+#         )
+#
+#         user.set_password(password)
+#         user.save(using=self._db)
+#         return user
+#
+#     def create_superuser(self, email, date_of_birth, password=None):
+#         """
+#         Creates and saves a superuser with the given email, date of
+#         birth and password.
+#         """
+#         user = self.create_user(
+#             email,
+#             password=password,
+#             date_of_birth=date_of_birth,
+#         )
+#         user.is_admin = True
+#         user.save(using=self._db)
+#         return user
+#
+#
+# class MyUser(AbstractBaseUser):
+#     email = models.EmailField(
+#         verbose_name='email address',
+#         max_length=255,
+#         unique=True,
+#     )
+#     date_of_birth = models.DateField()
+#     is_active = models.BooleanField(default=True)
+#     is_admin = models.BooleanField(default=False)
+#
+#     objects = MyUserManager()
+#
+#     USERNAME_FIELD = 'email'
+#     REQUIRED_FIELDS = ['date_of_birth']
+#
+#     def __str__(self):
+#         return self.email
+#
+    # def has_perm(self, perm, obj=None):
+    #     "Does the user have a specific permission?"
+    #     # Simplest possible answer: Yes, always
+    #     return True
+#
+#     def has_module_perms(self, app_label):
+#         "Does the user have permissions to view the app `app_label`?"
+#         # Simplest possible answer: Yes, always
+#         return True
+#
+#     @property
+#     def is_staff(self):
+#         "Is the user a member of staff?"
+#         # Simplest possible answer: All admins are staff
+#         return self.is_admin
