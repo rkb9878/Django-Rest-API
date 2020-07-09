ed88525b848f532d2dda9ff47e7eb74eab5a1ed7
Rahul kumar Bharti <rkb6280@gmail.com>
2020-07-09 09:27:05 +0530

    Added viewset to profile api

--- profiles_api/urls.py
+++ profiles_api/urls.py
@@ -1,7 +1,18 @@
-from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
+from rest_framework.routers import DefaultRouter
+
 from profiles_api import views
+
+router=DefaultRouter()
+router.register('hello-viewset',views.HelloViewSet,basename='hello-viewset')
+
 urlpatterns = [
+    # API view
     path('hello-view/',views.HelloApiView.as_view()),
-    path('s-view/',views.snippetView.as_view()),
+    path('s-view/', views.snippetView.as_view()),
+
+    # viewset
+    path('',include(router.urls)),
+
+
 ]
--- profiles_api/views.py
+++ profiles_api/views.py
@@ -3,7 +3,7 @@ from rest_framework.response import Response
 from rest_framework import status
 from profiles_api.models import snippet
 from profiles_api import serializers
-
+from rest_framework import viewsets
 
 class HelloApiView(APIView):
     """ Test API view"""
@@ -46,7 +46,56 @@ class HelloApiView(APIView):
 
     def delete(self,request,pk=None):
         """Delete an object"""
-        return Response({'method':'patch'})
+        return Response({'method':'Delete'})
+
+
+class HelloViewSet(viewsets.ViewSet):
+    """Test Api viewSet"""
+
+    serializer_class = serializers.helloSerializer
+
+    def list(self,request):
+        """Return hello message"""
+        a_viewSet=[
+            'using Action (list, create, retrive, update, partial_update, destroy',
+            'Automatically maps to urls using routing',
+            'Providing more Functionality with less code',
+        ]
+
+        return Response({'message':"Hello !!!!",'a_viewset':a_viewSet})
+
+    def create(self,request):
+        """Create a new Hello message"""
+        serializer=self.serializer_class(data=request.data)
+
+        if serializer.is_valid():
+            name=serializer.validated_data.get('name')
+            mobile=serializer.validated_data.get('mobile')
+
+            message=f"hello {name} and mobile number is {mobile}"
+
+            return Response({'message':message})
+        else:
+            return Response(
+                serializer.errors,
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+    def retrieve(self,request,pk=None):
+        """Handling getting an object by its ID"""
+        return Response({'Http_method':'GET'})
+
+    def update(self,request,pk=None):
+        """Handling update and object by its ID"""
+        return Response({"Http_methot": 'PUT'})
+
+    def partial_update(self,request,pk=None):
+        """Handling partial update of an object"""
+        return Response({'Http_method':'PATCH'})
+
+    def destroy(self,request,pk=None):
+        """Handling remove an object"""
+        return Response({'Http_method':'Delete'})
 
 
 class snippetView(APIView):
